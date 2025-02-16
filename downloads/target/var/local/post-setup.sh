338babc278109bfb0c22942d92138230074e8551
Akinori Furuta <afuruta@m7.dion.ne.jp>
2025-02-16 13:04:29 +0900

    Add files to fix target
    + Use frame buffer instead of drm.
    + Let systemd timeouts more longer.
    + Don't wait drm device.
    + Disable services they don't work well on emulator.

---
+++ downloads/target/var/local/post-setup.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Remove first run script, it contains password and pass phrase.
+rm -f /boot/firmware/firstrun.sh
+# Lock user rpi-first-boot-wizard
+sudo usermod -L rpi-first-boot-wizard
+# Disable services, they don't work well on emulator.
+sudo systemctl disable hciuart.service
+sudo systemctl disable ModemManager.service
+sudo systemctl disable rpi-eeprom-update.service
