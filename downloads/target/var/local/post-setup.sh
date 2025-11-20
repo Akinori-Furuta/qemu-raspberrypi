feb0c3ba372d189142e9e7200950390731aa23e1
Akinori Furuta <afuruta@m7.dion.ne.jp>
2025-11-20 21:08:10 +0900

    There is no hciuart.service
    * Is there successor of hciuart.service?

--- downloads/target/var/local/post-setup.sh
+++ downloads/target/var/local/post-setup.sh
@@ -6,6 +6,6 @@ rm -f /boot/firmware/firstrun.sh
 # Lock user rpi-first-boot-wizard
 sudo usermod -L rpi-first-boot-wizard
 # Disable services, they don't work well on emulator.
-sudo systemctl disable hciuart.service
+# sudo systemctl disable hciuart.service
 sudo systemctl disable ModemManager.service
 sudo systemctl disable rpi-eeprom-update.service
