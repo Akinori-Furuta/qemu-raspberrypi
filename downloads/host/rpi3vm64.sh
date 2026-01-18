8be98e3a699e398693c7cad681fa48d9d63ab60b
Akinori Furuta <afuruta@m7.dion.ne.jp>
2026-01-18 11:35:39 +0900

    Store kernel parameter in variable Append

--- downloads/host/rpi3vm64.sh
+++ downloads/host/rpi3vm64.sh
@@ -25,17 +25,20 @@ then
 	source "${CommonFile}"
 fi
 
+[[ -z "${Append}" ]] && \
+Append="console=ttyAMA1,115200 console=tty1\
+ root=/dev/mmcblk0p2 rootfstype=ext4 fsck.repair=yes rootwait\
+ dwc_otg.fiq_fsm_enable=0\
+ bcm2708_fb.fbwidth=1024 bcm2708_fb.fbheight=768\
+"
+
 qemu-system-aarch64 \
 -machine raspi3b \
 -kernel "${KernelFile}" \
 -initrd "${InitrdFile}" \
 -dtb "${DtbFile}" \
 -drive "${_DriveParam}" \
--append "console=ttyAMA1,115200 console=tty1\
- root=/dev/mmcblk0p2 rootfstype=ext4 fsck.repair=yes rootwait\
- dwc_otg.fiq_fsm_enable=0\
- bcm2708_fb.fbwidth=1024 bcm2708_fb.fbheight=768\
-" \
+-append "${Append}" \
 -serial mon:stdio \
 -no-reboot \
 -device usb-kbd \
