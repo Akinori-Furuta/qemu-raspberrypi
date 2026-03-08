8da9afbf039a20e4a1d32d3cb2845a441ea3daaa
Akinori Furuta <afuruta@m7.dion.ne.jp>
2026-03-09 00:32:00 +0900

    Implement load layered configuration files to script to start VNC server

--- downloads/host/rpi3vm64-vnc.sh
+++ downloads/host/rpi3vm64-vnc.sh
@@ -6,12 +6,47 @@
 MyWhich="$( which "$0" )"
 MyPath="$( readlink -f "${MyWhich}" )"
 MyDir="$( dirname "${MyPath}" )"
+MyDirArg0="$( dirname "${MyWhich}" )"
 MyBase="$( basename "$0" )"
 MyBody="${MyBase%.*}"
 MyBodyNoSuffix="${MyBody%%-*}"
 
-ConfigFile="${MyDir}/${MyBody}.conf"
 CommonFile="${MyDir}/${MyBodyNoSuffix}-common.sh"
+
+function LoadLayeredConfigFile() {
+	local	name_next
+	local	path
+	local	path_a0
+	local	path_rl
+	local	path_a0_rl
+
+	name_next="${1%-*}"
+	if [[ "${name_next}" != "$1" ]]
+	then
+		LoadLayeredConfigFile "${name_next}"
+	fi
+
+	path="${MyDir}/$1.conf"
+	if [[ -f "${path}" ]]
+	then
+		echo "$0: INFO: Load configuration file \"${path}\"."
+		source "${path}"
+	fi
+
+	path_a0="${MyDirArg0}/$1.conf"
+	if [[ -f "${path_a0}" ]]
+	then
+		path_rl="$( readlink -f "${path}" )"
+		path_a0_rl="$( readlink -f "${path_a0}" )"
+		if [[ "${path_rl}" != "${path_a0_rl}" ]]
+		then
+			echo "$0: INFO: Load configuration file \"${path_a0}\"."
+			source "${path_a0}"
+		fi
+	fi
+}
+
+LoadLayeredConfigFile "${MyBase%.*}"
 
 if [ -f "${ConfigFile}" ]
 then
